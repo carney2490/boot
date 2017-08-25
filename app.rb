14c9d5d7e3d7cbb38eb95f7a622f4bf1cc0f530b
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-08-25 03:02:39 -0400

    more changes

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 get '/' do
 
- "Welcome to MIKE SUCKS .COM Where the only thing more loserY is well nothing hmmmm "
+ "Welcome to MIKE SUCKS .COM Where the only thing more loserY is well nothing hmmmm did this work?"
 
 end

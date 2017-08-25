a187c506aab993fc5e4859b1901d02a77ee64ec6
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-08-25 02:50:13 -0400

    more changes

--- codedeploy/application_start.sh
+++ codedeploy/application_start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Fetch and install the cookbooks we depend on:
-cd /var/www/simple-app/chef
-librarian-chef install
-librarian-chef update
+#cd /var/www/simple-app/chef
+#librarian-chef install
+#librarian-chef update
 
 # Apply the chef recipes to this machine:
 #cd /var/www/simple-app

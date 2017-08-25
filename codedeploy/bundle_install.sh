b1467d36214bf43088f53cdf7b4b53e5425daf60
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2017-08-25 01:18:19 -0400

    revisons to see if it wokrs

--- codedeploy/bundle_install.sh
+++ codedeploy/bundle_install.sh
@@ -6,4 +6,4 @@ cd /var/www/simple-app
 BUNDLE_GEMFILE=/var/www/simple-app/Gemfile
 
 # Only start puma if bundle installation succeeds:
-bundle --deployment && bundle exec puma -t 0:16 -e staging -d
+bundle --deployment && bundle exec ruby app.rb
